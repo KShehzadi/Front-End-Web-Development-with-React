660d6a3da7cc96d60069b13b995e6bba7d409eb7
Komal303 <shehzadikomal303@gmail.com>
2020-08-19 19:02:18 +0500

    testing

--- Week 1/confusion/src/index.js
+++ Week 1/confusion/src/index.js
@@ -10,8 +10,14 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('myroot')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
