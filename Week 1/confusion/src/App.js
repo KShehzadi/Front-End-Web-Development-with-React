132ba7b1dbb2d3842a7cb6524cba3fccc7d9b732
Komal303 <shehzadikomal303@gmail.com>
2020-08-20 13:48:35 +0500

    configuring react

--- Week 1/confusion/src/App.js
+++ Week 1/confusion/src/App.js
@@ -2,19 +2,20 @@ import React,{ Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component{
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+class App extends Component {
   render() {
     return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
-      </header>
-    </div>
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
     );
   }
 }
+
 export default App;
--- Week 1/confusion/src/index.js
+++ Week 1/confusion/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,12 +11,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('myroot')
-);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
