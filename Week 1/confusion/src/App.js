1cb8b1956a1cdd3f0a6e4f6d711952c3bfed4664
Komal303 <shehzadikomal303@gmail.com>
2020-08-20 18:39:36 +0500

    React Component part 1

--- Week 1/confusion/src/App.js
+++ Week 1/confusion/src/App.js
@@ -3,11 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import Menu from './components/MenuComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
