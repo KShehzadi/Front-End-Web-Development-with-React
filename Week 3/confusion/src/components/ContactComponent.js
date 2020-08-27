ab5f302b3606e6f7a6ddca860488fe10bd5a80a9
Komal303 <shehzadikomal303@gmail.com>
2020-08-27 22:16:28 +0500

    Controlled Forms update 1

--- Week 3/confusion/src/components/ContactComponent.js
+++ Week 3/confusion/src/components/ContactComponent.js
@@ -150,7 +150,7 @@ class Contact extends Component {
                             <FormGroup row>
                                 <Col md={{size: 10, offset: 2}}>
                                     <Button type="submit" color="primary">
-                                        Send Feedback
+                                        Send Us Your Feedback
                                     </Button>
                                 </Col>
                             </FormGroup>
